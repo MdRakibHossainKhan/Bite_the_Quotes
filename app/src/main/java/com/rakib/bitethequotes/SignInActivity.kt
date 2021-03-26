5b485cf3a3fad69059949e7c5272690b5b44f19f
Md. Rakib Hossain Khan <md.rakib.hossain.khan@gmail.com>
2021-03-26 14:29:16 +0600

    Sign In & Sign Up UI Designed

---
+++ app/src/main/java/com/rakib/bitethequotes/SignInActivity.kt
@@ -0,0 +1,22 @@
+package com.rakib.bitethequotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        buttonSignIn.setOnClickListener {
+
+        }
+
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this@SignInActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
